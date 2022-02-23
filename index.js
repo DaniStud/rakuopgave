f7d2c3cf9247f1f47c767f575b3cf903d90c6145
daniel <dani041d@stud.kea.dk>
2022-02-23 20:14:15 +0100

    funktionelburger

--- index.js
+++ index.js
@@ -144,3 +144,35 @@ function visDetaljer(keramik) {
 
 modal.addEventListener("click", () => (modal.style.display = "none"));
 // hentdata();
+
+
+// burger menu 
+// Lav en variabel, der refererer til ".toggle-btn"
+const btn = document.querySelector(".toggle-btn");
+// Lav en variabel, der refererer til "nav"
+const headernav = document.querySelector("#headernav");
+
+// Lav en function, der hedder toggleMenu()
+function toggleMenu () {
+  console.log("toggleMenu")
+  // 1. Toggle en klasse på nav vha. classList.toggle
+  headernav.classList.toggle("shown")
+  // 2. Toggle en klasse, der hedder "shown"
+
+  // 1. Lav en variabel, der hedder menuShown
+  let menuShown = headernav.classList.contains("shown");
+  // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
+
+  // 1. Lav en if/else sætning => if (...) {...} else {...}
+  // 2. Spørg om menu i if-sætningen => if (menu)
+  if (menuShown) {
+     btn.textContent = "Luk";
+    // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
+  } else {
+    btn.textContent = "Menu";
+    // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
+  }
+}
+
+/* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
+btn.addEventListener("click", toggleMenu);
